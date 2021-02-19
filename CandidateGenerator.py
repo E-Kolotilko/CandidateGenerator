5bb1c31045324d567ce34531e6abef3795794f3e
E-Kolotilko <e.kolotilko@gmail.com>
2021-02-19 15:23:15 +0300

    Basic to_end_state generator added. More docs?

---
+++ CandidateGenerator.py
@@ -0,0 +1,49 @@
+
+class CandidateGenerator():
+    """Generate string based on vocabulary from some_state to all_last_chars state """
+    
+    def __init__(self, vocabulary, init_state):
+        """ Init indexes based on init_state string and vocabulary list"""
+        self._vocabulary_=vocabulary
+        self._init_state_=init_state
+        self.set_state(init_state)
+
+    def set_state(self, new_state):
+        """Reset indexes based on new_state string and existing vocabulary"""
+        self._indexes_=[]
+        for char in new_state:
+            self._indexes_.append(self._vocabulary_.index(char))
+
+    def generate(self):
+        """Increment and then return current state or None if end was reached"""
+        if self._increment_():
+            state = [self._vocabulary_[x] for x in self._indexes_]
+            return ''.join(state)
+        else:
+            return None
+        
+    def _increment_(self):
+        """Try to increment. Return True if possible else False"""
+        vocab_length = len(self._vocabulary_)
+        if self._indexes_[-1]>=vocab_length:
+            return False
+        self._indexes_[0]+=1
+        for i in range(len(self._indexes_)-1):
+            if self._indexes_[i]>=vocab_length:
+                self._indexes_[i]=0
+                self._indexes_[i+1]+=1
+            else:
+                break
+        return self._indexes_[-1]<vocab_length
+
+
+if __name__=="__main__":
+    symbols = [*range(48, 58), *range(65, 91), *range(97, 123)]
+    symbols = [chr(x) for x in symbols]
+    name = symbols[-3]+symbols[-1]*4
+    #name = symbols[-3]*3
+    generator = CandidateGenerator(symbols,name)
+    while (name):
+        print(name)
+        name=generator.generate()
+    print('Done')
